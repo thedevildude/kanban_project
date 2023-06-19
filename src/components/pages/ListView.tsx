15ba55eee4ffac6f85529736e1e861d8ae670f08
Devdeep Ghosh <devarmforce01@gmail.com>
2023-06-19 06:52:53 +0530

    task list updated

--- src/components/pages/ListView.tsx
+++ src/components/pages/ListView.tsx
@@ -11,29 +11,43 @@ type Props = {
 
 const ListView = (props: Props) => {
   return (
-    <div>
-      <div className="flex flex-col gap-5">
-        {props.tasks.map((task) => (
-          <div
-            key={task.id}
-            className="flex items-center p-5 bg-teal-200 hover:bg-teal-300 rounded-lg justify-between"
-          >
-            <div className="flex flex-col">
-              <h1 className="text-lg font-bold">{task.title}</h1>
-              <p className="text-sm">{task.description}</p>
-            </div>
-            <p>{task.status_object?.title}</p>
-            <div className="flex items-center gap-5">
-              <button onClick={() => props.deleteTaskCB(task.id)}>
-                <FaTrash />
-              </button>
-              <button onClick={() => props.setTaskEditor(true, task.id)}>
-                <BsThreeDots />
-              </button>
-            </div>
+    <div className="flex flex-col gap-5">
+      {props.tasks.map((task) => (
+        <div
+          key={task.id}
+          className="flex items-center p-5 bg-teal-200 hover:bg-teal-300 rounded-lg justify-between"
+        >
+          <div className="flex flex-col w-1/4 overflow-hidden">
+            <h1 className="text-lg font-bold">{task.title}</h1>
+            <p className="text-sm">{task.description}</p>
           </div>
-        ))}
-      </div>
+          <p>{task.status_object?.title}</p>
+          <div>
+            <p>
+              {
+                new Date(task.duedate ? task.duedate : "")
+                  .toISOString()
+                  .split("T")[0]
+              }
+            </p>
+            <p>
+              {
+                new Date(task.duedate ? task.duedate : "")
+                  .toTimeString()
+                  .split(" ")[0]
+              }
+            </p>
+          </div>
+          <div className="flex items-center gap-5">
+            <button onClick={() => props.deleteTaskCB(task.id)}>
+              <FaTrash />
+            </button>
+            <button onClick={() => props.setTaskEditor(true, task.id)}>
+              <BsThreeDots />
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
